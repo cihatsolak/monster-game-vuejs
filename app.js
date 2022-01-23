45291c5736daae636c9918f0cb59c7021e2e9675
DTEKNOLOJI\ExtCihatS <cihat.solak@d-teknoloji.com.tr>
2022-01-23 23:06:11 +0300

    Game rules added

--- app.js
+++ app.js
@@ -10,38 +10,54 @@ new Vue({
       this.game_is_on = true;
     },
     attack: function () {
-        let point = Math.ceil(Math.random() * 10);
-        this.monster_heal -= point;
-        this.monster_attack();
+      let point = Math.ceil(Math.random() * 10);
+      this.monster_heal -= point;
 
-        console.log(`You heal: ${this.player_heal}`);
-        console.log(`Monster heal: ${this.monster_heal}`);
+      this.monster_attack();
     },
     special_attack: function () {
-        let point = Math.ceil(Math.random() * 25);
-        this.monster_heal -= point;
-        this.monster_attack();
+      let point = Math.ceil(Math.random() * 25);
+      this.monster_heal -= point;
 
-        console.log(`You heal: ${this.player_heal}`);
-        console.log(`Monster heal: ${this.monster_heal}`);
+      this.monster_attack();
     },
     heal_up: function () {
-        let point = Math.ceil(Math.random() * 20);
-        this.player_heal += point;
-        this.monster_attack();
+      let point = Math.ceil(Math.random() * 50);
+      this.player_heal += point;
 
-        console.log(`You heal: ${this.player_heal}`);
-        console.log(`Monster heal: ${this.monster_heal}`);
+      this.monster_attack();
     },
     give_up: function () {
+      this.player_heal = 0;
+    },
+    monster_attack: function () {
+      let point = Math.ceil(Math.random() * 15);
+      this.player_heal -= point;
+    },
+  },
+  watch: {
+    player_heal: function (value) {
+      if (value <= 0) {
         this.player_heal = 0;
 
-        console.log(`You heal: ${this.player_heal}`);
-        console.log(`Monster heal: ${this.monster_heal}`);
+        let result = confirm("You lost the game. Do you want to try again?");
+        if (result) {
+          this.player_heal = 100;
+          this.monster_heal = 100;
+        }
+      } else if (value >= 100) {
+        this.player_heal = 100;
+      }
     },
-    monster_attack: function(){
-        let point = Math.ceil(Math.random() * 15);
-        this.player_heal -= point;
-    }
+    monster_heal: function (value) {
+      if (value <= 0) {
+        this.monster_heal = 0;
+        let result = confirm("You won the game. Do you want to try again?");
+        if (result) {
+          this.player_heal = 100;
+          this.monster_heal = 100;
+        }
+      }
+    },
   },
 });

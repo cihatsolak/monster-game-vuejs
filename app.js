be1833db1814ed353b61cd96e0b979ca62ba7ec0
DTEKNOLOJI\ExtCihatS <cihat.solak@d-teknoloji.com.tr>
2022-01-24 00:12:44 +0300

    Code refactoring for method and properties

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ new Vue({
     player_heal: 100,
     monster_heal: 100,
     game_is_on: false,
+    attack_multiple: 10,
+    special_attack_multiple: 25,
+    monster_attack_multiple: 15,
+    first_aid_multiple: 20,
+    log_text: {
+      attack: "Player attack: ",
+      special_attack: "Special player attack: ",
+      monster_attack: "Monster attack: ",
+      heal_up: "İlk yardım: ",
+      give_up: "Oyuncu Pes Etti!!!",
+    },
     logs: [],
   },
   methods: {
@@ -11,34 +22,48 @@ new Vue({
       this.game_is_on = true;
     },
     attack: function () {
-      let point = Math.ceil(Math.random() * 10);
+      let point = Math.ceil(Math.random() * this.attack_multiple);
       this.monster_heal -= point;
-      this.add_to_log({ turn: "p", text: `Player attack: ${point}` });
+      this.add_to_log({ turn: "p", text: `${this.log_text.attack} ${point}` });
       this.monster_attack();
     },
     special_attack: function () {
-      let point = Math.ceil(Math.random() * 25);
+      let point = Math.ceil(Math.random() * this.special_attack_multiple);
       this.monster_heal -= point;
-      this.add_to_log({ turn: "p", text: `Player special attack: ${point}` });
+      this.add_to_log({
+        turn: "p",
+        text: `${this.log_text.special_attack} ${point}`,
+      });
       this.monster_attack();
     },
     heal_up: function () {
-      let point = Math.ceil(Math.random() * 50);
+      let point = Math.ceil(Math.random() * this.first_aid_multiple);
       this.player_heal += point;
-      this.add_to_log({ turn: "p", text: `Player requested first aid: ${point}` });
+      this.add_to_log({
+        turn: "p",
+        text: `${this.log_text.heal_up} ${point}`,
+      });
       this.monster_attack();
     },
     give_up: function () {
       this.player_heal = 0;
-      this.add_to_log({ turn: "p", text: "The player gave up" });
+      this.add_to_log({ turn: "p", text: this.log_text.give_up });
     },
     monster_attack: function () {
-      let point = Math.ceil(Math.random() * 15);
+      let point = Math.ceil(Math.random() * this.monster_attack_multiple);
       this.player_heal -= point;
-      this.add_to_log({ turn: "m", text: `Monster attack: ${point}` });
+      this.add_to_log({
+        turn: "m",
+        text: `${this.log_text.monster_attack} ${point}`,
+      });
     },
     add_to_log: function (log) {
       this.logs.push(log);
+    },
+    resetConfigurations: function () {
+      this.player_heal = 100;
+      this.monster_heal = 100;
+      this.logs = [];
     },
   },
   watch: {
@@ -48,9 +73,7 @@ new Vue({
 
         let result = confirm("You lost the game. Do you want to try again?");
         if (result) {
-          this.player_heal = 100;
-          this.monster_heal = 100;
-          this.logs = [];
+          this.resetConfigurations();
         }
       } else if (value >= 100) {
         this.player_heal = 100;
@@ -61,11 +84,21 @@ new Vue({
         this.monster_heal = 0;
         let result = confirm("You won the game. Do you want to try again?");
         if (result) {
-          this.player_heal = 100;
-          this.monster_heal = 100;
-          this.logs = [];
+          this.resetConfigurations();
         }
       }
+    },
+  },
+  computed: {
+    player_progress: function () {
+      return {
+        width: this.player_heal + "%",
+      };
+    },
+    monster_progress: function () {
+      return {
+        width: this.monster_heal + "%",
+      };
     },
   },
 });

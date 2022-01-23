91bdd99f764c35e2da1d2fdf09a11be1428ce6ac
DTEKNOLOJI\ExtCihatS <cihat.solak@d-teknoloji.com.tr>
2022-01-23 22:48:40 +0300

    Button actions completed

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 new Vue({
-  al: "#app",
+  el: "#app",
   data: {
     player_heal: 100,
     monster_heal: 100,
+    game_is_on: false,
   },
   methods: {
-    start_game: function () {},
-    attack: function () {},
-    speacial_attack: function () {},
-    heal_up: function () {},
-    give_up: function () {}
+    start_game: function () {
+      this.game_is_on = true;
+    },
+    attack: function () {
+        let point = Math.ceil(Math.random() * 10);
+        this.monster_heal -= point;
+        this.monster_attack();
+
+        console.log(`You heal: ${this.player_heal}`);
+        console.log(`Monster heal: ${this.monster_heal}`);
+    },
+    special_attack: function () {
+        let point = Math.ceil(Math.random() * 25);
+        this.monster_heal -= point;
+        this.monster_attack();
+
+        console.log(`You heal: ${this.player_heal}`);
+        console.log(`Monster heal: ${this.monster_heal}`);
+    },
+    heal_up: function () {
+        let point = Math.ceil(Math.random() * 20);
+        this.player_heal += point;
+        this.monster_attack();
+
+        console.log(`You heal: ${this.player_heal}`);
+        console.log(`Monster heal: ${this.monster_heal}`);
+    },
+    give_up: function () {
+        this.player_heal = 0;
+
+        console.log(`You heal: ${this.player_heal}`);
+        console.log(`Monster heal: ${this.monster_heal}`);
+    },
+    monster_attack: function(){
+        let point = Math.ceil(Math.random() * 15);
+        this.player_heal -= point;
+    }
   },
 });

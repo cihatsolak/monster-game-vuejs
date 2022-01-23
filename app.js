1894ec88b399a9e0e2668a25bd97ae976bfe5d50
DTEKNOLOJI\ExtCihatS <cihat.solak@d-teknoloji.com.tr>
2022-01-23 22:24:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+  al: "#app",
+  data: {
+    player_heal: 100,
+    monster_heal: 100,
+  },
+  methods: {
+    start_game: function () {},
+    attack: function () {},
+    speacial_attack: function () {},
+    heal_up: function () {},
+    give_up: function () {}
+  },
+});
